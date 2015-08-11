0a5df95f33da9356b7b86bbfbaeadf00d6b8d202
Ads Fcv <ads.fcv.2014.2016@outlook.com>
2015-08-11 20:29:50 -0300

    Versao 3

--- EssaPorcaria/pageBD.aspx
+++ EssaPorcaria/pageBD.aspx
@@ -2,4 +2,22 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:DataGrid ID="CadastrosDataGrid" 
+    runat="server"
+    CellPadding ="3"
+    CellSpancing="0"
+    Font-Names="CourierNew"
+    Font-Size="10pt"
+    HeaderStyle-BackColor="#aaaadd"
+    AllowPaging="false"
+    AllowSorting="false"
+    PagSize="50"
+    PagerStyle-Position="TopAndBottom"                                            
+    PagerStyle-Font-Bold="true"
+    PagerStyle-Font-Size="12pt"      
+    PagerStyle-BackColor="#c0c0c0"/>  
+    />
+    
+
+
 </asp:Content>
--- EssaPorcaria/pageBD.aspx.cs
+++ EssaPorcaria/pageBD.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace EssaPorcaria
 {
@@ -11,7 +14,35 @@ namespace EssaPorcaria
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+             SqlConnection conexao = new SqlConnection(""
+                 +"DaTa Source=(LocalDb\\v11.0;AttachDbFilename="
+                 +"D:\\Github\\EssaPorcaria\\EssaPorcaria\\BD\\northwind.mdf;Integrated Security=True");
+            /*   
+               SqlConnection conexao = new SqlConnection(""
+                   +"server=MARCELL\\MARCELL;"
+                   +"Database=northwind;"
+                   +"Integrated Security=true");
 
+            */
+               //Criando o comando sql para executar no banco informado na conexão
+               SqlCommand comandoSql = new SqlCommand("SELECT * FROM CADASTROS", conexao);
+
+               //Criando o SqlDataAdapter para executar a query e preencher o dataset
+               SqlDataAdapter adaptador = new SqlDataAdapter(comandoSql);
+
+               //Criando o dataset e preenchendo o mesmo com o resultado da execução da query
+               DataSet ds = new DataSet();
+               adaptador.Fill(ds);
+               //Criando o dataview e associando a origem dos dados
+               DataView source = new DataView();
+               source.Table = ds.Tables[0];
+
+               //Criando a conexão entre o conjunto de dados (dataset) e o componente visual (datagrid)
+               CadastrosDataGrid.DataSource = source;
+
+               //Montando e exibindo as informações no grid
+               CadastrosDataGrid.DataBind();
+               
         }
     }
 }
